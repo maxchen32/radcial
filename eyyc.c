b82576ee4e37b5c56aec680582f92b1dee4770c8
chenhaifeng <chenhaifeng@bmsoft.com.cn>
2021-05-02 20:33:43 +0800

    more examples

---
+++ eyyc.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "radical.h"
+#include "fraction.h"
+
+void eyyc(int a, int b, int c);
+int main(int argc, char * argv[]) {
+    eyyc(1, 2, 1);
+    return 0;
+}
+
+void eyyc(int a, int b, int c){     //f(x)=ax^2+bx+c
+    Polynomial list = initPoly();
+    Polynomial list2 = initPoly();
+    int delta = b * b - 4 * a * c;
+    if (delta >= 0){
+        Radical nb = initRad(-b, 1, 1);
+        list = addRad(addRad(list, nb), Radsqrt(delta));
+        int da = 2 * a;
+        _PolyIntReduce(list, &da);
+        printPoly(list);
+        printf("\n");
+        if (da != 1)
+            printf("%d\n",da);
+
+        da = 2 * a;
+        list2 = subRad(addRad(list2, nb), Radsqrt(delta));
+        _PolyIntReduce(list2, &da);
+        printPoly(list2);
+        printf("\n");
+        if (da != 1)
+            printf("%d\n",da);
+    }
+    else {
+        printf("delta = %d < 0\n",delta);
+    }
+
+    destoryPoly(list2);
+    destoryPoly(list);
+}
---
+++ xyline.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <math.h>
+#include "fraction.h"
+#include "radical.h"
+
+typedef enum {
+	OK,
+	NOROOT,
+	INFROOT
+}Status;
+Status xyline(Fraction* res, Fraction a, Fraction b, Fraction m, 
+  Fraction c, Fraction d, Fraction n) {
+	Fraction h1 = subFrac(mulFrac(a, d), mulFrac(b, c));
+	Fraction t1 = subFrac(mulFrac(d, m), mulFrac(b, n));
+	Fraction t2 = subFrac(mulFrac(a, n), mulFrac(c, m));
+
+	if (h1.up == 0 && t1.up != 0) {
+		return NOROOT;
+	} else if(h1.up != 0 && t2.up == 0) {
+		return INFROOT;
+	} else {
+		res[0] = divFrac(t1, h1);
+		res[1] = divFrac(t2, h1);
+		return OK;
+	}
+}
+int main(){
+	Fraction result[2];
+	Status sta = xyline(result, initFrac(1,1), initFrac(1,1), initFrac(10,1), 
+	  initFrac(64,56), initFrac(1,1), initFrac(52,5));
+	if (sta == OK) {
+		printFrac(result[0], "\n");
+		printFrac(result[1], "\n");
+	} else if (sta == NOROOT) {
+		printf("No Root\n");
+	} else {
+		printf("Inf Root\n");
+	}
+	return 0;
+}
